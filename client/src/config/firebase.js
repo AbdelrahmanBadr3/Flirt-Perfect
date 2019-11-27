d75be879ea708d03e851241258a6fd9f03978b0c
NadineMoustafa <nadine.moustafaa@gmail.com>
2019-11-27 11:03:39 +0200

    Feature_ sign in With google

---
+++ client/src/config/firebase.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyBTpxC3sQ2I_VlGz896Mjz6x9T9lkj0sDc",
+    authDomain: "flirt-perfect-a8963.firebaseapp.com",
+    databaseURL: "https://flirt-perfect-a8963.firebaseio.com",
+    projectId: "flirt-perfect-a8963",
+    storageBucket: "flirt-perfect-a8963.appspot.com",
+    messagingSenderId: "601269195341",
+    appId: "1:601269195341:web:994de335081fb96d4ac204",
+    measurementId: "G-SP3L6DL9YG"
+  
+  }
